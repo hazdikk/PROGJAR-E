2deaa614ed483d806100e5fabddeafb60e94855f
hazdik <m.hazdik@gmail.com>
2019-03-27 00:23:57 +0700

    belom pake thread

--- Tugas 2/serverudp.py
+++ Tugas 2/serverudp.py
@@ -1,21 +1,23 @@
 import socket
 
 
-SERVER_IP = '127.0.0.1'
+SERVER_IP = ['127.0.0.1', '127.0.0.2', '127.0.0.3', '127.0.0.4']
 SERVER_PORT = 9000
-NAMAFILE='new.jpg'
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((SERVER_IP, SERVER_PORT))
-
-fp = open(NAMAFILE,'wb+')
-ditulis=0
-
-while True:
-    data, addr = sock.recvfrom(1024)
-    print "blok ", len(data), data[0:10]
-    fp.write(data)
+NAMAFILE=['gunter.jpg']
+folder='client'
 
 
+for IP in SERVER_IP:
+    for file in NAMAFILE:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((IP, SERVER_PORT))
 
-fp.close()
+        fp = open(folder + '-' + IP + '/' + file,'wb+')
+        ditulis=0
+
+        while True:
+            data, addr = sock.recvfrom(1024)
+            print "blok ", len(data), data[0:10]
+            fp.write(data)
+
+        fp.close()
