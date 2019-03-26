96d1f444c86ad8a3edf45bce58e331f4c54373d7
hazdik <33057079+hazdikk@users.noreply.github.com>
2019-03-26 22:58:01 +0700

    Add files via upload

---
+++ Tugas 2/serverudp.py
@@ -0,0 +1,21 @@
+import socket
+
+
+SERVER_IP = '127.0.0.3'
+SERVER_PORT = 9000
+NAMAFILE='new.png'
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((SERVER_IP, SERVER_PORT))
+
+fp = open(NAMAFILE,'wb+')
+ditulis=0
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print "blok ", len(data), data[0:10]
+    fp.write(data)
+
+
+
+fp.close()
