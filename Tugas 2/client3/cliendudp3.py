c6dddb4501999b20f2da45e9af3bae11a96366d1
hazdik <m.hazdik@gmail.com>
2019-03-27 06:53:05 +0700

    done tugas2

---
+++ Tugas 2/client3/cliendudp3.py
@@ -0,0 +1,26 @@
+import socket
+import select
+
+CLIENT_IP = "127.0.0.3"
+CLIENT_PORT = 9000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((CLIENT_IP, CLIENT_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    if data:
+        print "menerima file ", data
+        file_name = data
+
+    f = open(file_name, 'wb')
+
+    while True:
+        ready = select.select([sock], [], [], 5)
+        if ready[0]:
+            data, addr = sock.recvfrom(10240)
+            f.write(data)
+        else:
+            print "%s telah diterima" % file_name
+            f.close()
+            break
--- Tugas 2/serverudp.py
+++ Tugas 2/serverudp.py
@@ -1,23 +1,26 @@
 import socket
+import time
+import sys
 
-
-SERVER_IP = ['127.0.0.1', '127.0.0.2', '127.0.0.3', '127.0.0.4']
+SERVER_IP = ["127.0.0.1","127.0.0.2","127.0.0.3","127.0.0.4",];
 SERVER_PORT = 9000
-NAMAFILE=['gunter.jpg']
-folder='client'
+buf = 1024
+NAMAFILE= ['gunter.jpg', 'bart.png']
 
+for IP in SERVER_IP: 
+	for file in NAMAFILE: 
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-for IP in SERVER_IP:
-    for file in NAMAFILE:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((IP, SERVER_PORT))
+		sock.sendto(file, (IP, SERVER_PORT))
+		print "mengirim %s" % file
 
-        fp = open(folder + '-' + IP + '/' + file,'wb+')
-        ditulis=0
+		f = open(file, "rb")
+		data = f.read()
+		while(data):
+		    if(sock.sendto(data, (IP, SERVER_PORT))):
+		        data = f.read()
 
-        while True:
-            data, addr = sock.recvfrom(1024)
-            print "blok ", len(data), data[0:10]
-            fp.write(data)
-
-        fp.close()
+		sock.close()
+		f.close()
+		time.sleep(10)
